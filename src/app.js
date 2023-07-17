559b19a51bb33309801d507f4e212e533540a1c9
Mculler25 <mdculler2@gmail.com>
2023-07-17 14:55:58 -0700

    added CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
@@ -9,6 +10,7 @@ const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewRouter);
